4dfba1e0d91f8996982095f87d580418bdc46ad9
slotish <slotish@.gmail.com>
2016-10-26 21:20:19 +0200

    adding new view styling and github module

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.middleware.use OmniAuth::Builder do
 	provider :facebook, '670109949829215', '2afe2704b7563ec4a6c85ae6df752eb1'
+	provider :github, '576d1e88c23562256237', '11862adeefd8e31af863044978d63b2e34d3a568'
 end
